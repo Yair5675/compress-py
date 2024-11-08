400b8ce1597f4ff6c5e97d76b7be1e7357352754
Yair5675 <yairziv5675@gmail.com>
2024-11-08 22:20:41 +0200

    Changed the 'turn_identifiers_into_bytes' method to use HuffmanEncoding instead of int

--- compressors/huffman/identifiers.py
+++ compressors/huffman/identifiers.py
@@ -79,29 +79,28 @@ def get_identifiers_from_bytes(bit_stream: bytes) -> tuple[dict[HuffmanEncoding,
     return identifiers, bit_idx
 
 
-def turn_identifiers_into_bytes(identifiers: dict[bytes, int]) -> bytes:
+def turn_identifiers_into_bytes(identifiers: dict[bytes, HuffmanEncoding]) -> bytes:
     """
-    Given a dictionary that maps byte values from 0 to 255 to shorter identifiers, the function produces a bit stream
-    that contains those identifiers. The bit stream is saved in python as a bytes object.
-    The stream's length will vary depending on the identifiers, in order to save space as much as possible.
+    Given a dictionary that maps byte values from 0 to 255 to huffman encodings, the function produces a bit stream
+    that contains those encodings. The bit stream is saved in python as a bytes object.
+    The stream's length will vary depending on the encodings, in order to save space as much as possible.
 
     The stream's format is the following:
-    The first byte will always contain the number of identifiers encoded. There are 256 possible identifiers (in case
-    all byte values are encoded), so the number of identifiers in the stream will be the first byte's value, plus one.
-    Note that this doesn't allow for 0 identifiers. If an empty dictionary is given to the function, the result will
+    The first byte will always contain the number of encodings in the stream. There are 256 possible encodings (in case
+    all byte values are encoded), so the number of encodings in the stream will be the first byte's value, plus one.
+    Note that this doesn't allow for 0 encodings. If an empty dictionary is given to the function, the result will
     always be an empty bytes object.
 
-    The next byte value is the identifier key (the value that is encoded).
-    The next half a byte (4 bits) will hold the length of the encoded identifier in BITS (not bytes). 4 bits allows
-    for a maximum of 15 bits, or 32768 possible values for the identifier. This is enough values while also being pretty
+    The next byte value is the original byte's value (the value that is encoded).
+    The next half a byte (4 bits) will hold the length of the huffman encoding in BITS (not bytes). 4 bits allows
+    for a maximum of 15 bits, or 32768 possible values for the encoding. This is enough values while also being pretty
     compact in terms of memory.
-    The next bits will be the actual encoded value, and after them either the stream ends or the next identifier will be
-    stored.
+    The next bits will be the actual huffman encoding, and after them either the stream ends or the next identifier will
+    be stored.
 
-    :param identifiers: A dictionary mapping regular byte values to shorter values, which are stored as an integer for
-                        simpler bit operations.
-    :return: A bytes object that encodes these identifiers.
-    :raises TypeError: If the argument isn't a dictionary mapping bytes to integers.
+    :param identifiers: A dictionary mapping regular byte values to huffman encodings.
+    :return: A bytes object that encodes these encodings.
+    :raises TypeError: If the argument isn't a dictionary mapping bytes to HuffmanEncoding objects.
     :raises ValueError: If the amount of entries exceeds 256, or if the key of one of the entries contains multiple
                         bytes (only one is allowed).
     """
@@ -126,32 +125,31 @@ def turn_identifiers_into_bytes(identifiers: dict[bytes, int]) -> bytes:
     return bytes(bit_buffer)
 
 
-def __insert_identifier_to_buffer(byte_val: bytes, short_encoding: int, buffer: BitBuffer) -> None:
+def __insert_identifier_to_buffer(byte_val: bytes, short_encoding: HuffmanEncoding, buffer: BitBuffer) -> None:
     """
-    Inserts the bits of the given encoding into the bit buffer.
+    Inserts the bits of the given huffman encoding into the bit buffer, according to the formatting of huffman encodings
+    as bits.
     :param byte_val: The byte value that is encoded.
     :param short_encoding: The short encoding given to the byte value.
-    :param buffer: The buffer that the identifier will be inserted into.
+    :param buffer: The buffer that the encoding will be inserted into.
     """
     # Insert the byte value:
     buffer.insert_byte(byte_val)
 
     # Insert the number of bits the short encoding takes up as a 4 bit value:
-    identifier_bits_len = max(1, short_encoding.bit_length() & 0xF)
+    identifier_bits_len = short_encoding.bit_length & 0xF
     for i in range(3, -1, -1):
         buffer.insert_bit((identifier_bits_len >> i) & 1)
 
-    # Insert the actual bits of the value:
-    for i in range(identifier_bits_len - 1, -1, -1):
-        buffer.insert_bit((short_encoding >> i) & 1)
+    # Insert the actual bits of the encoding:
+    short_encoding.load_to_buffer(buffer)
 
 
-def __validate_identifiers_dict(identifiers: dict[bytes, int]) -> None:
+def __validate_identifiers_dict(identifiers: dict[bytes, HuffmanEncoding]) -> None:
     """
-    Validates the type and length of the identifier dictionary.
-    :param identifiers: A dictionary mapping regular byte values to shorter values, which are stored as an integer for
-                        simpler bit operations.
-    :raises TypeError: If the argument isn't a dictionary mapping bytes to integers.
+    Validates the type and length of the 'encodings' dictionary.
+    :param identifiers: A dictionary mapping regular byte values to HuffmanEncoding objects.
+    :raises TypeError: If the argument isn't a dictionary mapping bytes to HuffmanEncoding objects.
     :raises ValueError: If the amount of entries exceeds 256, or if the key of one of the entries contains multiple
                         bytes (only one is allowed).
     """
@@ -161,7 +159,7 @@ def __validate_identifiers_dict(identifiers: dict[bytes, int]) -> None:
 
     # Check entries count:
     elif len(identifiers) > 256:
-        raise ValueError(f"The identifiers dictionary can only contain 256 entries max ({len(identifiers)} received)")
+        raise ValueError(f"The encodings dictionary can only contain 256 entries max ({len(identifiers)} received)")
 
     # Check key, value types:
     for key, value in identifiers.items():
@@ -172,5 +170,5 @@ def __validate_identifiers_dict(identifiers: dict[bytes, int]) -> None:
             raise ValueError(f"Dictionary keys should be one byte long (got {len(key)})")
 
         # Value check:
-        if not isinstance(value, int):
-            raise TypeError(f"Dictionary should use `int` as value type (got {type(value)} instead)")
+        if not isinstance(value, HuffmanEncoding):
+            raise TypeError(f"Dictionary should use `HuffmanEncoding` as value type (got {type(value)} instead)")
