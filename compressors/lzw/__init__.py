fb45fcd8811a35b7a5062f9107b865f50142fc4e
Yair5675 <yairziv5675@gmail.com>
2024-11-12 18:43:11 +0200

    Extracted the logic behind parsing input data into indices and padding them out of the __init__.py file and into a new file called lzw_indices.py

--- compressors/lzw/__init__.py
+++ compressors/lzw/__init__.py
@@ -1,86 +1,23 @@
 from typing import Generator
 from collections import deque
 from compressors import Compressor
-from .encoding_dict import EncodingDict, TooManyEncodingsException
+from .lzw_indices import EncodingIndices
+from .encoding_dict import TooManyEncodingsException
 
 
 class LzwCompressor(Compressor):
     __slots__ = [
-        # The dictionary used for encoding data:
-        '__encoder_dict'
+        # The maximum amount of entries that can be saved inside an LZW dictionary:
+        '__max_dict_size',
     ]
 
     def __init__(self, max_dict_size: int):
-        # Set the encoder dictionary:
-        self.__encoder_dict = EncodingDict(max_dict_size)
+        if not isinstance(max_dict_size, int):
+            raise TypeError(f"Expected max_dict_size of type int, got {type(max_dict_size)} instead")
+        elif max_dict_size <= 0:
+            raise ValueError(f"max_dict_size must be positive (got {max_dict_size})")
 
-    def __get_encoder_indices(self, input_data: bytes) -> deque[int]:
-        """
-        Parses the input data and converts it to a list of indices inside an encoder dictionary.
-        Calling this method will change the encoder dictionary saved in the object, as previous
-        indices cannot be used on the new data.
-        :param input_data: The input data before being compressed.
-        :return: A deque of indices inside an encoder dictionary.
-        :raises TooManyEncodingsException: If not enough memory was given to the encoding dictionary
-                                           in order to complete the algorithm.
-        """
-        # Clear previous indices:
-        self.__encoder_dict.clear()
-
-        # The starting index of the slice that matches a dictionary value:
-        matching_start_idx: int = 0
-        output: deque[int] = deque()
-
-        for i in range(1, len(input_data)):
-            # Get the current data slice:
-            current_data = input_data[matching_start_idx:i + 1]
-
-            # If it's not in the dictionary, insert the index of the previous slice, and
-            # add the new slice to the dictionary:
-            if not self.__encoder_dict.contains_key(current_data):
-                prev_matching = input_data[matching_start_idx:i]
-                output.append(self.__encoder_dict[prev_matching])
-
-                self.__encoder_dict.insert(current_data)  # This can raise a memory exception
-                matching_start_idx = i
-
-        # In case the end of the input matched a dictionary value, the loop didn't append it:
-        forgotten = input_data[matching_start_idx:]
-        if len(forgotten) > 0:
-            output.append(self.__encoder_dict[forgotten])
-
-        return output
-
-    @staticmethod
-    def __pad_encoded_indices(indices: deque[int]) -> bytes:
-        """
-        When compressing the indices, the decompression algorithm needs to know where an index starts and end.
-        One approach is to decide on a fixed size for all of them, however this wastes space.
-        The current function pads the indices in the following way:
-            Before every index, an extra byte is added. This byte will tell the decompression algorithm
-            how many bytes to read for the next index.
-        This method adds only one extra byte per index, which is more efficient than the other approach
-        mentioned above.
-        :param indices: A list of the indices that will be padded and turned into a bytes object.
-        :return: A bytes object containing the given indices along with extra bytes to allow the decompression
-                 algorithm to read the indices.
-        """
-        # For every index, extract only its necessary bytes (get rid of unnecessary 0 bits):
-        def get_important_bytes(n: int) -> bytes:
-            if n == 0:
-                return bytes(1)
-            important_bytes = []
-            while n > 0:
-                important_bytes.append(n & 0xFF)
-                n >>= 8
-            return bytes(important_bytes)  # Most significant byte last (little endian)
-        necessary_indices_bytes = map(get_important_bytes, indices)
-
-        # Add an extra byte for length:
-        padded_indices = map(lambda index_bytes: bytes([len(index_bytes)]) + index_bytes, necessary_indices_bytes)
-
-        # Concatenate it all:
-        return b''.join(padded_indices)
+        self.__max_dict_size = max_dict_size
 
     def encode(self, input_data: bytes) -> bytes:
         """
@@ -96,10 +33,10 @@ class LzwCompressor(Compressor):
             raise TypeError(f'Expected type bytes, got {type(input_data)} instead')
 
         # Parse indices:
-        indices = self.__get_encoder_indices(input_data)
+        indices = EncodingIndices(input_data, self.__max_dict_size)
 
         # Pad and return them:
-        return LzwCompressor.__pad_encoded_indices(indices)
+        return indices.get_padded_bytes()
 
     @staticmethod
     def encoded_indices_iterator(compressed_data: bytes) -> Generator[int, None, None]:
@@ -152,7 +89,7 @@ class LzwCompressor(Compressor):
 
         for encoded_idx in LzwCompressor.encoded_indices_iterator(compressed_data):
             # Check for memory limitations:
-            if encoded_idx - 256 > self.__encoder_dict.max_size:
+            if encoded_idx - 256 > self.__max_dict_size:
                 raise TooManyEncodingsException()
 
             # Check if the index is in the dictionary (smaller than unoccupied_idx), or is ascii:
---
+++ compressors/lzw/lzw_indices.py
@@ -0,0 +1,77 @@
+from collections import deque
+from .encoding_dict import EncodingDict
+
+
+class EncodingIndices:
+    """
+    A class encapsulating the logic behind converting input data into indices inside an LZW dictionary.
+    """
+    __slots__ = (
+        # The indices of the encoded data inside the lzw dictionary:
+        '__indices',
+    )
+
+    def __init__(self, input_data: bytes, max_dict_size: int) -> 'EncodingIndices':
+        """
+        Parses the input data into encoding indices inside an LZW dictionary.
+        :param input_data: The input that will be compressed into indices.
+        :param max_dict_size: The maximum amount of entries the LZW dictionary can contain.
+        :raises TooManyEncodingsException: If not enough memory was given to the encoding dictionary
+                                           in order to complete the algorithm.
+        """
+        # Initialize the dictionary:
+        lzw_dict: EncodingDict = EncodingDict(max_dict_size)
+
+        # The starting index of the slice that matches a dictionary value:
+        matching_start_idx: int = 0
+        self.__indices: deque[int] = deque()
+
+        for i in range(1, len(input_data)):
+            # Get the current data slice:
+            current_data = input_data[matching_start_idx:i + 1]
+
+            # If it's not in the dictionary, insert the index of the previous slice, and
+            # add the new slice to the dictionary:
+            if not lzw_dict.contains_key(current_data):
+                prev_matching = input_data[matching_start_idx:i]
+                self.__indices.append(lzw_dict[prev_matching])
+
+                lzw_dict.insert(current_data)  # This can raise a memory exception
+                matching_start_idx = i
+
+        # In case the end of the input matched a dictionary value, the loop didn't append it:
+        forgotten = input_data[matching_start_idx:]
+        if len(forgotten) > 0:
+            self.__indices.append(lzw_dict[forgotten])
+
+    @property
+    def indices(self) -> deque[int]:
+        return self.__indices
+
+    def get_padded_bytes(self) -> bytes:
+        """
+        When compressing the indices, the decompression algorithm needs to know where an index starts and end.
+        One approach is to decide on a fixed size for all of them, however this wastes space.
+        The current method pads the indices as bytes in the following way:
+            Before every index, an extra byte is added. This byte will tell the decompression algorithm
+            how many bytes to read for the next index.
+        This method adds only one extra byte per index, which is more efficient than the other approach mentioned above.
+        :return: A bytes object containing the indices in the object, along with extra bytes to allow the decompression
+                 algorithm to read the indices.
+        """
+        # For every index, extract only its necessary bytes (get rid of unnecessary 0 byts):
+        def get_important_bytes(n: int) -> bytes:
+            if n == 0:
+                return bytes(1)
+            important_bytes = []
+            while n > 0:
+                important_bytes.append(n & 0xFF)
+                n >>= 8
+            return bytes(important_bytes)  # Most significant byte last (little endian)
+        necessary_indices_bytes = map(get_important_bytes, self.indices)
+
+        # Add an extra byte for length:
+        padded_indices = map(lambda index_bytes: bytes([len(index_bytes)]) + index_bytes, necessary_indices_bytes)
+
+        # Concatenate it all:
+        return b''.join(padded_indices)
