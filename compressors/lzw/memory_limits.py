4f68e235aa541f605921db0f6ba47b678d589a99
Yair5675 <yairziv5675@gmail.com>
2024-11-13 23:18:59 +0200

    Created an enum holding different strategies in case the LZW dictionary runs out of memory. Those strategies aren't implemented yet

--- compressors/lzw/memory_limits.py
+++ compressors/lzw/memory_limits.py
@@ -1,3 +1,6 @@
+from enum import Enum, auto
+
+
 class TooManyEncodingsException(Exception):
     """
     A custom exception that is raised when the dictionary used in the LZW algorithm exceeds its maximum size.
@@ -5,3 +8,20 @@ class TooManyEncodingsException(Exception):
     def __init__(self, message="The dictionary reached its maximum size"):
         self.message = message
         super().__init__(message)
+
+
+class OutOfMemoryStrategy(Enum):
+    """
+    During the LZW algorithm, the dictionary's size may not be sufficient. In this case, the program needs to know how
+    to proceed.
+    This enum contains different strategies to handle this case.
+    """
+    # The simplest strategy - if it ever happens, abort the execution:
+    ABORT = auto()
+
+    # If we run out of memory, stop storing new entries. This hurts compression efficiency:
+    STOP_STORE = auto()
+
+    # Add just enough memory to complete the algorithm. In practice - this means incrementing the maximum size by one
+    # whenever we exceed it:
+    USE_MINIMUM_REQUIRED = auto()
