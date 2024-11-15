696439a6706a20c9a1c932ca2a8850915ae53312
Yair5675 <yairziv5675@gmail.com>
2024-11-15 16:53:41 +0200

    Modified BenchmarkResults to also contain data about compression efficiency and the interval between memory measurements

--- cli/benchmark.py
+++ cli/benchmark.py
@@ -1,4 +1,5 @@
 import pstats
+from typing import Optional
 from cProfile import Profile
 from functools import partial
 from dataclasses import dataclass
@@ -12,13 +13,33 @@ class BenchmarkResults:
     runtime_results: pstats.FunctionProfile
 
     # Memory usage over time:
+    memory_interval: float
     memory_usage_over_time: list[float]
     max_mem: float
     min_mem: float
     avg_mem: float
 
-    def __init__(self, runtime_data: pstats.FunctionProfile, memory_data: list[float]):
+    # Results regarding compression efficiency:
+    compression_ratio: Optional[float]
+    space_saving: Optional[float]
+
+    def __init__(
+            self, runtime_data: pstats.FunctionProfile, memory_data: list[float], memory_interval: float,
+            data_size: Optional[tuple[int, int]]
+    ) -> 'BenchmarkResults':
+        """
+        Initializes the BenchmarkResults object.
+        :param runtime_data: Data regarding the performance of the algorithm, represented as a pstats.FunctionProfile
+                             object.
+        :param memory_data: A sequence of memory usage values. This sequence represent the memory usage of the function,
+                            measured at different intervals (time difference between each measurement is the
+                            'memory_interval' parameter).
+        :param memory_interval: The time interval between each memory measurement.
+        :param data_size: A tuple containing the original size of the data and the compressed size of the data, in this
+                          order. This parameter only makes sense when compressing, hence why it is optional.
+        """
         # Set attributes:
+        self.memory_interval = memory_interval
         self.runtime_results = runtime_data
         self.memory_usage_over_time = memory_data
 
@@ -35,6 +56,13 @@ class BenchmarkResults:
         else:
             self.avg_mem /= len(self.memory_usage_over_time)
 
+        # Calculate compression ratio and space saving
+        self.compression_ratio = None
+        self.space_saving = None
+        if data_size is not None:
+            self.compression_ratio = data_size[0] / data_size[1]  # Uncompressed / compressed
+            self.space_saving = 1 - (data_size[1] / data_size[0])  # 1 - (compressed / uncompressed)
+
 
 class CompressorBenchmark:
     """
@@ -47,7 +75,7 @@ class CompressorBenchmark:
 
     def __init__(self, compressor: Compressor) -> 'CompressorBenchmark':
         if not isinstance(compressor, Compressor):
-            raise TypeError(f"Expected compressor of type Comressor, got {type(compressor)} instead")
+            raise TypeError(f"Expected compressor of type Compressor, got {type(compressor)} instead")
 
         self.__comp = compressor
 
@@ -61,14 +89,17 @@ class CompressorBenchmark:
         :param compress: Whether the benchmark should compress or decompress the input data.
         """
         # Profile runtime and memory:
+        MEMORY_INTERVAL = 1e-3
         method_to_check = partial(self.compressor.encode if compress else self.compressor.decode, input_data)
         with Profile() as runtime_profiler:
-            (memory_usage_data, output) = memory_usage(proc=method_to_check, interval=1e-2, retval=True)
+            (memory_usage_data, output) = memory_usage(proc=method_to_check, interval=MEMORY_INTERVAL, retval=True)
 
-        # Create the results:
+        # Create the results (add data about compression if we encode):
         method_name = 'encode' if compress else 'decode'
         runtime_data = pstats.Stats(runtime_profiler).get_stats_profile().func_profiles[method_name]
-        results = BenchmarkResults(runtime_data, memory_usage_data)
+
+        data_size = (len(input_data), len(output)) if compress else None
+        results = BenchmarkResults(runtime_data, memory_usage_data, MEMORY_INTERVAL, data_size)
 
         return output, results
 
