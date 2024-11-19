effec95e2877023380830dac0d721c3606489265
Yair5675 <yairziv5675@gmail.com>
2024-11-19 21:59:19 +0200

    Instead of having a static method to generate probability intervals, the class now holds them as an attribute, wrapped around a custom type (namedtuple called ProbabilityInterval)

--- compressors/arithmetic.py
+++ compressors/arithmetic.py
@@ -1,40 +1,45 @@
-from collections import Counter
+from collections import namedtuple
 from compressors import Compressor
+
+# A type representing a probability interval inside the arithmetic compressor. Since we are working with integer
+# math only, it will be represented as a cumulative frequency, and total cumulative frequency. True probability
+# can be calculated as `prob_0 = start_cum // tot_cum, prob_1 = end_cum // tot_cum`:
+ProbabilityInterval = namedtuple('ProbabilityInterval', (
+    # Cumulative frequency up until this interval:
+    'start_cum',
+    # Cumulative frequency up until this interval, including it:
+    'end_cum',
+    # Cumulative frequency of all intervals:
+    'tot_cum'
+))
 
 
 class ArithmeticCompressor(Compressor):
-    @staticmethod
-    def get_cumulative_frequency_ranges(input_data: bytes) -> list[tuple[int, int]]:
+
+    __slots__ = (
+        # The probability intervals that define the arithmetic compression:
+        'prob_intervals',
+    )
+
+    def __init__(self) -> 'ArithmeticCompressor':
+        self.__init_equal_probs()
+
+    def __init_equal_probs(self) -> None:
         """
-        Given a bytes object representing input data, this function computes and returns the cumulative frequency ranges
-        for each byte value in the input data.
-
-        The cumulative frequency range for a byte value 'i' is the interval [low, high), where:
-        - 'low' is the cumulative frequency of byte value 'i-1' (or 0 if 'i' is 0).
-        - 'high' is the cumulative frequency of byte value 'i'.
-
-        The cumulative frequency of a byte value is defined as the sum of the frequencies of all byte values smaller
-        than or equal to that byte value. The cumulative frequency range provides a way to map each byte to a specific
-        interval in the range [0, total frequency of all bytes].
-
-        :param input_data: A bytes object containing the input data. The frequency of each byte value is calculated
-                           to determine the cumulative frequency ranges.
-        :return: A list of length 256, where each element at index 'i' is a tuple (low, high).
-                 The tuple represents the cumulative frequency range for byte value 'i'.
-                 - 'low' is the cumulative frequency of byte 'i-1' (or 0 for byte 0).
-                 - 'high' is the cumulative frequency of byte 'i'.
+        Assigns a list of probability intervals to attribute 'prob_intervals'.
+        The assigned intervals are equal in length, which means the compressor assumes each byte value is equally
+        likely. Granted, adaptive probability intervals may produce better compression efficiency.
+        The intervals list will be of length 257:
+            - First 256 elements represent the intervals corresponding to all byte values.
+            - Last element is an additional 'EOF' value.
         """
-        # Count the occurrence of every byte value:
-        counter = Counter(input_data)
+        # Total cumulative frequency is 257:
+        tot_cum = 257
 
-        # Form the cumulative frequencies list:
-        cumulative_frequencies: list[tuple[int, int]] = [(0, 0)] * 256
-        cumulative_frequencies[0] = 0, counter[0]
-        for byte_val in range(1, len(cumulative_frequencies)):
-            prev_cum = cumulative_frequencies[byte_val - 1]
-            cumulative_frequencies[byte_val] = prev_cum[1], prev_cum[1] + counter[byte_val]
-
-        return cumulative_frequencies
+        # Build probability intervals with equal probabilities:
+        self.prob_intervals: tuple[ProbabilityInterval] = tuple(
+            ProbabilityInterval(i, i + 1, tot_cum) for i in range(257)
+        )
 
     def encode(self, input_data: bytes) -> bytes:
         """
@@ -42,8 +47,7 @@ class ArithmeticCompressor(Compressor):
         :param input_data: The bytes that will be encoded.
         :return: A compressed version of 'input_data'.
         """
-        # Get the cumulative frequency of each byte value:
-        cumulative_frequencies: list[tuple[int, int]] = ArithmeticCompressor.get_cumulative_frequency_ranges(input_data)
+        pass
 
     def decode(self, compressed_data: bytes) -> bytes:
         pass
