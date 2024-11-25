a5fa39cec26c5545f48533928735de695b72ac3b
Yair5675 <yairziv5675@gmail.com>
2024-11-25 19:07:21 +0200

    Created a method that initializes the cumulative-frequency-intervals (CFI) dictionary and total frequency count based on the input data. When doing that, it limits the total frequency possible to have half the bits of the bits system, ensuring no overlaps between intervals will exist

--- compressors/arithmetic/encoder.py
+++ compressors/arithmetic/encoder.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from compressors.arithmetic.bits_system import BitsSystem
 
 
@@ -15,14 +16,17 @@ class Encoder:
         # The number of pending bits from near-convergence situations during encoding:
         'pending_bits',
 
-        # A dictionary mapping between an input value (byte value or EOF) and its cumulative frequency interval:
-        'cum_freqs'
+        # Cumulative Frequency Intervals - a mapping between an input value and its interval inside the total cumulative
+        # frequency of the input:
+        'cfi',
+
+        # Total frequency of the input data:
+        'total_freq'
     )
 
     def __init__(self, eof: int, bits_system: BitsSystem) -> 'Encoder':
         self.bits_system = bits_system
         self.eof = eof
-        self.cum_freqs: dict[int, tuple[int, int]]
 
     @property
     def low(self):
@@ -39,3 +43,30 @@ class Encoder:
     @width.setter
     def width(self, value):
         self.__width = value & self.bits_system.MAX_CODE
+
+    def init_cum_freqs(self, input_data: bytes) -> None:
+        """
+        Initializes the cumulative frequency dictionary and the total frequency attributes according to the input data.
+        """
+        # Count all byte value occurrences, but limit the maximum frequency to have half the bits used by the bits
+        # system (ensuring all byte values can always be encoded with no interval overlap):
+        TOTAL_FREQ_LIMIT = 1 << (self.bits_system.BITS_USED // 2)
+        FREQ_PER_BYTE_LIMIT = TOTAL_FREQ_LIMIT // 256
+
+        counter: defaultdict[int, int] = defaultdict(int)
+        for byte_val in input_data:
+            if counter[byte_val] < FREQ_PER_BYTE_LIMIT:
+                counter[byte_val] = counter[byte_val] + 1
+
+        # Add the EOF value:
+        counter[self.eof] = 1
+
+        # Loop over the elements, and add their cumulative frequencies inside the dictionary:
+        prev_cum_freq = 0
+        self.cfi: dict[int, tuple[int, int]] = {}
+        for value, freq in counter.items():
+            self.cfi[value] = prev_cum_freq, prev_cum_freq + freq
+            prev_cum_freq += freq
+
+        # The total frequency is now saved in prev_cum_freq:
+        self.total_freq = prev_cum_freq
