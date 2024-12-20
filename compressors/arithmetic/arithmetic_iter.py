1b8c6e4366f70d1b63701af21d08f3e9017c5c32
Yair5675 <yairziv5675@gmail.com>
2024-12-20 21:26:47 +0200

    Changed the method in the StateCallback interface from 'process_state' to 'process_interval', and removed the 'state' parameter from it. The state can be easily computed from the interval itself in O(1), so providing it with the interval is just silly

--- compressors/arithmetic/arithmetic_iter.py
+++ compressors/arithmetic/arithmetic_iter.py
@@ -9,18 +9,16 @@ class StateCallback(ABC):
     """
 
     @abstractmethod
-    def process_state(self, interval: interval_state.Interval, state: interval_state.IntervalState) -> bool:
+    def process_interval(self, interval: interval_state.Interval) -> bool:
         """
         Depending on an interval and its state, the method will perform different actions.
         Note that the interval given as parameter should be treated as mutable, and may change after the method call.
 
-        After the method is executed, it returns a boolean. If true - the method's caller should re-compute the
-        interval's state and call the method again (it indicates the interval was mutated). If false - do not call it
-        again right after the method is finished.
+        After the method is executed, it returns a boolean. If true - the method's caller should call the method again
+        (it indicates the interval was mutated). If false - do not call it again right after the method is finished.
         :param interval: The interval that the state belongs to. It should be treated as mutable.
-        :param state: The state of the interval, will help decide what actions to take.
         :return: True if the interval was mutated and the method should be called on it (and its new state) again, False
-                 otherwise.
+                 if it should not be called again until something else updates it.
         """
         pass
 
@@ -58,4 +56,4 @@ class ArithmeticIterator:
         # Call the callback until it returns false:
         use_callback = True
         while use_callback:
-            use_callback = self.state_callback.process_state(self.current_interval, self.current_interval.get_state())
+            use_callback = self.state_callback.process_interval(self.current_interval)
