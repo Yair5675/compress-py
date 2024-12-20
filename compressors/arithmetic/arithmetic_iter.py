b971082ef06c79fa962adcda32a8d194cf908da0
Yair5675 <yairziv5675@gmail.com>
2024-12-20 21:23:49 +0200

    Added the main method in 'ArithmeticIterator': It receives a probability interval, updates the interval saved in the object according to it, and continuously calls the callback given to it, until the callback returns False

--- compressors/arithmetic/arithmetic_iter.py
+++ compressors/arithmetic/arithmetic_iter.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from compressors.arithmetic import interval_state
+from compressors.arithmetic.frequency_table import ProbabilityInterval
 
 
 class StateCallback(ABC):
@@ -41,3 +42,20 @@ class ArithmeticIterator:
     def __init__(self, start_interval: interval_state.Interval, state_callback: StateCallback) -> 'ArithmeticIterator':
         self.current_interval: interval_state.Interval = start_interval
         self.state_callback: StateCallback = state_callback
+
+    def process_prob_interval(self, probability_interval: ProbabilityInterval) -> None:
+        """
+        Given a probability interval, the method will update the interval saved inside the object, and call the provided
+        callback on it. The number of time the callback will be called depends on the callback's return value.
+        :param probability_interval: A ProbabilityInterval object that represents a section in the current interval.
+                                     The interval saved inside the iterator will be scaled to that probability interval,
+                                     and its state will be processed by the callback given during the object's
+                                     initialization.
+        """
+        # Update the interval:
+        self.current_interval.update(probability_interval)
+
+        # Call the callback until it returns false:
+        use_callback = True
+        while use_callback:
+            use_callback = self.state_callback.process_state(self.current_interval, self.current_interval.get_state())
