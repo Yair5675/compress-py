7bfb6d99c99548feff0ea6b9f18ed7c9f67449f3
Yair5675 <yairziv5675@gmail.com>
2025-01-11 20:14:40 +0200

    Used python's ProcessPoolExecutor to speed up the comparison (each algorithm encodes the data in its own core)

--- cli/comparisons.py
+++ cli/comparisons.py
@@ -1,28 +1,31 @@
+import time
 import rich
 import typer
-from rich import box
-from rich.table import Table
-
 import cli.lzw
+import functools
+from rich import box
 from pathlib import Path
+from rich.table import Table
 from compressors import Compressor
 from typing_extensions import Annotated
 from compressors.rle import RleCompressor
 from compressors.huffman import HuffmanCompressor
+from concurrent.futures import ProcessPoolExecutor
 from compressors.lzw import LzwCompressor, OutOfMemoryStrategy
 from cli.benchmark import CompressorBenchmark, BenchmarkResults
 
 compressors_to_test: tuple[tuple[str, Compressor], ...] = (
     ("Huffman Coding", HuffmanCompressor()),
-    ("LZW (best compression rate)", LzwCompressor(int(cli.lzw.DictionarySize.EXTRA_LARGE), OutOfMemoryStrategy.USE_MINIMUM_REQUIRED)),
+    ("LZW (best compression rate)",
+     LzwCompressor(int(cli.lzw.DictionarySize.EXTRA_LARGE), OutOfMemoryStrategy.USE_MINIMUM_REQUIRED)),
     ("LZW (medium memory usage)", LzwCompressor(int(cli.lzw.DictionarySize.MEDIUM), OutOfMemoryStrategy.STOP_STORE)),
     ("LZW (smallest memory usage)", LzwCompressor(int(cli.lzw.DictionarySize.SMALL), OutOfMemoryStrategy.STOP_STORE)),
     ("RLE", RleCompressor())
 )
 
 
-def test_with(input_path: Path, compressor: Compressor) -> BenchmarkResults:
-    benchmark = CompressorBenchmark(compressor)
+def test_with(input_path: Path, test_idx: int) -> BenchmarkResults:
+    benchmark = CompressorBenchmark(compressors_to_test[test_idx][1])
     with open(input_path, 'rb') as input_file:
         results: tuple[bytes, BenchmarkResults] = benchmark(input_file.read(), compress=True)
     return results[1]
@@ -30,14 +33,15 @@ def test_with(input_path: Path, compressor: Compressor) -> BenchmarkResults:
 
 def get_info_table(results: tuple[BenchmarkResults]) -> Table:
     info_table: Table = Table(
-        title="Combined Benchmark Results", style="bold blue", title_style="bold white", box=box.ROUNDED, show_lines=True
+        title="Combined Benchmark Results", style="bold blue", title_style="bold white", box=box.ROUNDED,
+        show_lines=True
     )
     info_table.add_column('Algorithm')
     info_table.add_column('Total Time (s)')
     info_table.add_column('Average Memory Usage (MiB)')
     info_table.add_column('Compression Ratio')
     info_table.add_column('Space Saving')
-    
+
     for i, result in enumerate(results):
         data = [compressors_to_test[i][0], f"{result.runtime_results.cumtime:.4f}", f"{result.avg_mem:.2f}"]
 
@@ -59,10 +63,13 @@ def compare_all(input_path: Annotated[Path, typer.Argument(
     Pay attention this command only compares the algorithms on the file, and does not produce an output file.
     """
     # Benchmark all algorithms:
-    results: tuple[BenchmarkResults] = tuple(map(
-        lambda compressor_to_test: test_with(input_path, compressor_to_test[1]), compressors_to_test)
-    )
-
+    start = time.perf_counter()
+    with ProcessPoolExecutor(max_workers=4) as executor:
+        results: tuple[BenchmarkResults] = tuple(executor.map(
+            functools.partial(test_with, input_path), range(len(compressors_to_test))
+        ))
+    end = time.perf_counter()
+    print(end - start)
     # Create a big table for all the algorithms:
     info_table = get_info_table(results)
     rich.print(info_table)
