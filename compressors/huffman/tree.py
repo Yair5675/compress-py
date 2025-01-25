51da611bf26a2484eba7df1bf7529013e8db3dec
Yair5675 <yairziv5675@gmail.com>
2025-01-25 18:43:20 +0200

    Added methods to serialize and deserialize HuffmanTree objects, which will use less space than the current serialization of encodings. Also added tests for the HuffmanTree class

--- compressors/huffman/tree.py
+++ compressors/huffman/tree.py
@@ -1,6 +1,14 @@
+import util
+from collections import deque
 from queue import PriorityQueue
+from util.bitbuffer import BitBuffer
 from typing import Optional, Sequence
 from compressors.huffman.identifiers import HuffmanEncoding
+
+
+class InvalidTreeFormat(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
 
 
 class HuffmanTree:
@@ -30,11 +38,20 @@ class HuffmanTree:
             self.char, self.frequency = char, frequency
             self.left, self.right = left, right
 
+        def __eq__(self, other: 'HuffmanTree.Node') -> bool:
+            if other is None:
+                return False
+            elif not isinstance(other, HuffmanTree.Node):
+                return False
+            else:
+                return (self.char == other.char and self.frequency == other.frequency and self.right == other.right and
+                        self.left == other.left)
+
         # Define 'less than' in order to use the Node class in a priority queue:
         def __lt__(self, other: 'HuffmanTree.Node') -> bool:
             return self.frequency < other.frequency
 
-        def __str__(self):
+        def __repr__(self):
             if self.char is None:
                 return str(self.left) + str(self.right)
             else:
@@ -46,6 +63,17 @@ class HuffmanTree:
             :return: True if the current node has no children, False otherwise.
             """
             return self.left is None and self.right is None
+
+        def get_leaves_count(self):
+            """
+            Calculates the number of leaves that the current node is connected to.
+            :return: Number of leaves children of the current node.
+            """
+            return (
+                    int(self.is_leaf()) +  # Current node
+                    (self.left.get_leaves_count() if self.left else 0) +  # Left child
+                    (self.right.get_leaves_count() if self.right else 0)  # Right child
+            )
 
         @property
         def char(self) -> Optional[bytes]:
@@ -145,7 +173,7 @@ class HuffmanTree:
         # Get the root (if all byte frequencies were zero, the queue is empty and the root is None):
         self.__root: Optional['HuffmanTree.Node'] = None if nodes_queue.empty() else nodes_queue.get_nowait()
 
-    def get_encodings(self: 'HuffmanTree') -> dict[bytes, int]:
+    def get_encodings(self: 'HuffmanTree') -> dict[bytes, HuffmanEncoding]:
         """
         Given the current huffman tree, the method assigns each byte value inside it a huffman encoding - a potentially
         shorter value based on the structure of the tree.
@@ -170,6 +198,151 @@ class HuffmanTree:
 
         dfs(self.root, 0, 0)
         return encodings
+
+    def to_bits(self) -> BitBuffer:
+        """
+        When using the huffman tree, one might want to include it along with any data compressed using it. Therefor,
+        this method encodes the tree in the most memory efficient way possible.
+        :return: A BitBuffer object containing the serialized huffman tree.
+        """
+        # Initialize the buffer:
+        output = BitBuffer()
+
+        # Ensure root isn't None:
+        if self.root is None:
+            return output
+
+        # Insert the number of leaves in the tree (minus one) as the first byte:
+        output.insert_bits(self.root.get_leaves_count() - 1, 8)
+
+        # Encode the tree using preorder traversal. Encode the byte value of the node (if it is a leaf node) or 0 if it
+        # isn't, then another two bits for the two possible children. 0 means no child, 1 means a child exists.
+        def preorder(current_node: Optional['HuffmanTree.Node']):
+            if current_node is not None:
+                # If it's a leaf, output the byte value and 00:
+                if current_node.is_leaf():
+                    output.insert_bits(current_node.char[0], 8)
+                    output.insert_bits(0, 2)
+                # If it isn't, place 0 instead of the actual byte value, and 1 or 0 for possible children:
+                else:
+                    output.insert_bits(0, 8)
+                    output.insert_bits(int(current_node.left is not None), 1)  # Left child
+                    output.insert_bits(int(current_node.right is not None), 1)  # Right child
+
+                    # Continue to the next:
+                    preorder(current_node.left)
+                    preorder(current_node.right)
+
+        preorder(self.root)
+        return output
+
+    @staticmethod
+    def __parse_nodes_data(compressed_data: bytes) -> tuple[list[tuple['HuffmanTree.Node', bool, bool]], int]:
+        """
+        Given data which whose start is the 'to_bits' method result, this method parses it into information about the
+        encoded Huffman tree's nodes.
+        Specifically, the function returns a list where every element in that list is the following:
+            (the node's value: int, left child exists: bool, right child exists: bool)
+        Note that this list represents a preorder traversal of the tree.
+        Additionally, the function returns the index of the first bit which WASN'T used in the creation of this list.
+        That index should be considered the data start index.
+        :param compressed_data: Data prefixed by the result of the 'to_bits' method.
+        :return: A list of node information and the data start index.
+        """        
+        if len(compressed_data) == 0:
+            return []
+        
+        nodes_info: list[tuple['HuffmanTree.Node', bool, bool]] = []
+        leaves_count = compressed_data[0] + 1
+        bit_offset = 8
+        
+        node_space = 10  # Number of bits a single node takes up
+        while leaves_count > 0 and bit_offset + node_space <= 8 * len(compressed_data):
+            # Read node's info:
+            value = util.read_bits(compressed_data, bit_offset, 8)
+            bit_offset += 8
+            has_left, has_right = util.get_bit(compressed_data, bit_offset), util.get_bit(compressed_data, bit_offset + 1)
+            bit_offset += 2
+            
+            # Append node and decrease the number of leaves remaining if necessary:
+            nodes_info.append((HuffmanTree.Node(bytes([value]), 0, None, None), has_left, has_right))
+            leaves_count -= 1 - (has_left | has_right)
+        
+        # If not all leaves were found, we have invalid data:
+        if leaves_count > 0:
+            raise InvalidTreeFormat(f"Expected {compressed_data[0]} leaves to be found, got {compressed_data[0] - leaves_count} instead")
+        
+        return nodes_info, bit_offset
+    
+    @staticmethod
+    def __form_root_node(nodes_info: list[tuple['HuffmanTree.Node', bool, bool]]) -> 'HuffmanTree.Node':
+        """
+        Given a list of information about a HuffmanTree's nodes in preorder traversal, the method reconstructs the 
+        tree's structure and returns its root node, which is connected to the fully reconstructed tree.
+        :param nodes_info: A list with information about each node in the tree. It is expected that this information
+                           was recorded in preorder traversal of the tree. Each element in the list is the following:   
+                            (the node itself, whether the node had a left child, whether the node had a right child)
+        :return: The root node of a HuffmanTree, constructed according to the given nodes' information.
+        """
+        # Start the tree with a dummy node:
+        dummy = HuffmanTree.Node(None, 0, None, None)
+        nodes_info.insert(0, (dummy, True, False))  # The actual tree will be the left child of dummy
+
+        parents_idx_stack: deque[HuffmanTree.Node] = deque()
+        parents_idx_stack.append(0)
+
+        for node_idx, (current_node, has_left, has_right) in enumerate(nodes_info[1:], start=1):
+            # Check if the stack is empty at this point — if so, raise an exception:
+            if not parents_idx_stack:
+                raise InvalidTreeFormat("Unexpectedly ran out of parent nodes. The tree structure is malformed.")
+
+            # Get parent info:
+            parent_node, parent_has_left, parent_has_right = nodes_info[parents_idx_stack[0]]
+
+            # Insert current node to either left or right of parent:
+            if parent_has_left and parent_node.left is None:
+                parent_node.left = current_node
+                # If we set the left child, we remove the parent only if we don't need to set the right child:
+                remove_parent = not parent_has_right
+
+            elif parent_has_right:
+                parent_node.right = current_node
+                # If we set the right child, we either didn't need to set the left one or did already:
+                remove_parent = True
+
+            # Remove parent from the stack if they don't need more children:
+            if remove_parent:
+                parents_idx_stack.popleft()
+
+            # Add the current node's index to the parents stack only if it has children:
+            if has_left or has_right:
+                parents_idx_stack.appendleft(node_idx)
+
+        # Remove dummy:
+        root = dummy.left
+        dummy.left = None
+        return root
+
+    @staticmethod
+    def from_bits(compressed_data: bytes) -> tuple['HuffmanTree', int]:
+        """
+        Given some compressed data, the method constructs a HuffmanTree object from it.
+        The data is expected to start with the output of the 'to_bits' method, any other format will result in a
+        nonsensical tree at best, or an exception at worst.
+        The method won't necessarily parse the entire data. Once the tree was fully constructed, the index of the first
+        bit which doesn't belong to the tree's serialization will be returned, along with the tree itself.
+        :return: The tree deserialized from the data, and the data start index.
+        """
+        # Parse the info:
+        nodes_info, data_start = HuffmanTree.__parse_nodes_data(compressed_data)
+        
+        # Get the root node:
+        root = HuffmanTree.__form_root_node(nodes_info)
+        
+        # Construct the tree object:
+        tree = HuffmanTree.__new__(HuffmanTree)
+        tree.__root = root
+        return tree, data_start
 
     @property
     def root(self) -> Optional['HuffmanTree.Node']:
---
+++ tests/test_huffman_tree.py
@@ -0,0 +1,106 @@
+import pytest
+from typing import Optional
+from collections import Counter
+from util.bitbuffer import BitBuffer
+from compressors.huffman.tree import HuffmanTree, HuffmanEncoding
+
+
+def get_tree_from_data(data: bytes) -> HuffmanTree:
+    frequencies: Counter = Counter(data)
+    frequencies: list[int] = [frequencies[i] for i in range(256)]
+
+    return HuffmanTree(frequencies)
+
+
+def same_structure(n1: Optional[HuffmanTree.Node], n2: Optional[HuffmanTree.Node]) -> bool:
+    """Checks if the structure of the roots is similar"""
+    n1_none, n2_none = n1 is None, n2 is None
+    if n1_none or n2_none:
+        return n1_none and n2_none
+    return same_structure(n1.right, n2.right) and same_structure(n1.left, n2.left)
+
+
+def same_leaves(n1: Optional[HuffmanTree.Node], n2: Optional[HuffmanTree.Node]) -> bool:
+    """Checks if the byte value of the leaves in the trees are the same"""
+    n1_none, n2_none = n1 is None, n2 is None
+    if n1_none or n2_none:
+        return n1_none and n2_none
+
+    if n1.is_leaf() ^ n2.is_leaf():  # One is a leaf and other isn't
+        return False
+
+    if n1.is_leaf():
+        return n1.char == n2.char
+
+    return same_leaves(n1.left, n2.left) and same_leaves(n1.right, n2.right)
+
+
+@pytest.fixture
+def example_tree() -> HuffmanTree:
+    return get_tree_from_data(b"aaaaaaaabbbbbbbccccdd")
+
+
+@pytest.fixture
+def example_tree_and_encodings(example_tree) -> tuple[HuffmanTree, dict[bytes, HuffmanEncoding]]:
+    encodings = {
+        b'a': HuffmanEncoding(1, 0),
+        b'b': HuffmanEncoding(2, 0b11),
+        b'c': HuffmanEncoding(3, 0b101),
+        b'd': HuffmanEncoding(3, 0b100)
+    }
+    return example_tree, encodings
+
+@pytest.fixture
+def example_tree_and_buffer(example_tree) -> tuple[HuffmanTree, BitBuffer]:
+    expected_tree_bits: int = 0b000000110000000011011000010000000000110000000011011001000001100011000110001000
+    buffer = BitBuffer()
+    buffer.insert_bits(expected_tree_bits, 78)
+    return example_tree, buffer
+
+
+@pytest.fixture
+def example_tree_and_nodes_info(example_tree) -> tuple[HuffmanTree, tuple[list[tuple[HuffmanTree.Node, bool, bool]], int]]:
+    byte_0 = bytes([0])
+    nodes_info = [
+        (HuffmanTree.Node(byte_0, 0, None, None), 1, 1),
+        (HuffmanTree.Node(b'a', 0, None, None), 0, 0),
+        (HuffmanTree.Node(byte_0, 0, None, None), 1, 1),
+        (HuffmanTree.Node(byte_0, 0, None, None), 1, 1),
+        (HuffmanTree.Node(b'd', 0, None, None), 0, 0),
+        (HuffmanTree.Node(b'c', 0, None, None), 0, 0),
+        (HuffmanTree.Node(b'b', 0, None, None), 0, 0)
+    ]
+    return example_tree, (nodes_info, 78)
+
+
+def test_to_bits(example_tree_and_buffer):
+    tree, expected_bits = example_tree_and_buffer
+    assert tree.to_bits() == expected_bits
+
+
+def test_parse_nodes_data(example_tree_and_nodes_info):
+    tree, nodes_info = example_tree_and_nodes_info
+    given_info = HuffmanTree._HuffmanTree__parse_nodes_data(bytes(tree.to_bits()))
+
+    assert given_info == nodes_info
+
+
+def test_form_root_node(example_tree_and_nodes_info):
+    tree, (nodes_info, _) = example_tree_and_nodes_info
+    constructed_root = HuffmanTree._HuffmanTree__form_root_node(nodes_info)
+    
+    assert same_leaves(tree.root, constructed_root) and same_structure(tree.root, constructed_root)
+    
+    
+def test_from_bits(example_tree):
+    org_encodings = example_tree.get_encodings()
+    
+    deserialized_tree, _ = HuffmanTree.from_bits(bytes(example_tree.to_bits()))
+    deserialized_encodings = deserialized_tree.get_encodings()
+    
+    assert org_encodings == deserialized_encodings
+
+
+def test_get_encodings(example_tree_and_encodings):
+    tree, encodings = example_tree_and_encodings
+    assert tree.get_encodings() == encodings
