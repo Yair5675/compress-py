4f8fe66e7218d93aa243c1852407637f8d10012e
Yair5675 <yairziv5675@gmail.com>
2025-03-05 23:05:00 +0200

    Previous division of data into blocks worked, but was inefficient: 1) Performing BWT on each block might group similar characters worse than calculating it on the whole data. 2) Metadata added to the BWTs was dependent on the data's length, whereas current implementation adds constant data (at most 257 bytes, but only for extremely large files) Implemented BWT for the entire data along with the more efficient metadata handling

--- transformations/bwt.py
+++ transformations/bwt.py
@@ -1,58 +1,63 @@
-from typing import Optional
 import transformations.suffix_arr as suffix_arr
 
 
-# The EOF character separating between the blocks' various EOF values and the blocks themselves:
-METADATA_EOF: int = 0
+def find_missing_byte_val(byte_values: bytes) -> int:
+    """
+    Given a collection of bytes whose length is at most 255, the function returns a byte value not present within the
+    given bytes.
+    Such value ought to exists, as there are 256 possible byte values yet the given bytes' length is at most 255.
+    :param byte_values: A collection of bytes, the returned value will not be contained in this collection. Its length
+                        is at most 255.
+    :return: A byte value not present within `byte_values`.
+    """
+    # Check length:
+    if len(byte_values) > 255:
+        raise ValueError(f"Length of given bytes must be less than 256 ({len(byte_values)} given)")
 
-
-class BWTBlock:
-    __slots__ = (
-        # Byte data stored in the block
-        'data',
-        # The EOF character in this particular block:
-        'eof'
-    )
-    # Maximum number of bytes in the block. Has to be 254 to ensure there are always 2 values which don't appear in the
-    # data, and one of them is NOT the external EOF character (used by the whole file):
-    MAX_BLOCK_SIZE: int = 254
+    # Initialize an INTEGER B with 256 bytes which are all 1:
+    B: int = (1 << 256) - 1
     
-    # Set of all byte values, pre-computed:
-    __BYTE_VALUES_SET: set[int] = set(range(256))
+    for byte_val in byte_values:
+        # Use the byte value as an offset from the least significant bit of B and get the bit at that offset:
+        b = (B >> byte_val) & 1
+        
+        # Turn this bit into 0 in B:
+        B ^= b << byte_val
+    
+    # Use int.bit_length() to get the offset of the most significant bit in B which is still a 1 (there must be at least
+    # one due to byte_values' length constraints:
+    return B.bit_length() - 1
 
-    def __init__(self, data: bytes, external_eof: Optional[int] = None) -> 'BWTBlock':
-        if len(data) > BWTBlock.MAX_BLOCK_SIZE:
-            raise ValueError(f"Data size must be less than or equal to {BWTBlock.MAX_BLOCK_SIZE} ({len(data)} given)")
-        self.data: bytes = data
 
-        # Find all missing values, and pick the one that isn't the external EOF as the block's EOF:
-        missing: set[int] = BWTBlock.__BYTE_VALUES_SET - set(data)
-
-        # Since there are 256 possible byte values, and at most 254 of them are in the data, it is guaranteed that at
-        # least 2 unique values are missing:
-        m1, m2 = missing.pop(), missing.pop()
-        self.eof: int = m1 if external_eof is None or m2 == external_eof else m2
-
-    def __len__(self) -> int:
-        return len(self.data)
-
-    def get_bwt(self) -> bytes:
-        """
-        Computes the BWT of the current block.
-        """
-        # Initialize a bytearray whose length is equal to the block's length:
-        result = bytearray(len(self) + 1)
-
-        # Form the suffix array, and shrink the alphabet size to include only the block's length excluding EOF:
-        suffix_array: list[int] = suffix_arr.build_sorted_suffix_array(self.data)
-
-        # Use the indices inside the suffix array to get the last value of data's rotation to that index:
-        for byte_idx, sorted_suffix_offset in enumerate(suffix_array):
-            if sorted_suffix_offset > 0:
-                result[byte_idx] = self.data[sorted_suffix_offset - 1]
-            else:
-                result[byte_idx] = self.eof
-        return bytes(result)
+def get_bwt_metadata(eof_offset: int) -> bytearray:
+    """
+    Given the offset of the EOF character inside the transformation, the function returns the necessary metadata to 
+    allow the inverse algorithm to reconstruct the original data.
+    :param eof_offset: The offset of the EOF character inside the transformation. This offset is assumed to be less than
+                        2 to the 2040 power (which I think is a fair request).
+    :return: Metadata which should be added before the transformation, without any delimiter in between.
+    """
+    # The metadata will be structured as follows:
+    # 1) Assuming the EOF index we need to store is smaller than 2 ** (8 * 255), it can be represented using 255 bytes
+    #    or fewer.
+    # 2) Due to this assumption, there will always be at least one byte value which does not appear in the EOF index's
+    #    bytes (as mentioned, it is composed of at most 255 bytes, and there are 256 unique byte values).
+    # 3) Call this unique byte value that does not appear in the EOF index M. Place M as the first byte of the message
+    #    to let the inverse algorithm know what the metadata EOF is.
+    # 4) Follow the first M with the EOF index's bytes (big endian), and insert M at the end again to signal the index's
+    #    end.
+    # This approach supports gigantic file sizes. Even if the base assumption limits the file size, it limits it to such
+    # an enormous number we should never realistically break it.
+    # Moreover, this approach allows us to transmit the EOF index through the data with constant memory addition (at
+    # the worst case, 257 bytes are added, but in such a case this amount is dwarfed by the data's size anyway).
+    eof_index_bytes: bytes = eof_offset.to_bytes((eof_offset.bit_length() + 7) // 8, byteorder='big')
+    missing_byte_val: int = find_missing_byte_val(eof_index_bytes)
+    
+    metadata: bytearray = bytearray(2 + len(eof_index_bytes))
+    metadata[0], metadata[-1] = missing_byte_val, missing_byte_val
+    metadata[1:-1] = eof_index_bytes
+    
+    return metadata
 
 
 def compute_bwt(data: bytes) -> bytes:
@@ -63,37 +68,22 @@ def compute_bwt(data: bytes) -> bytes:
     :param data: Some data whose BWT will be calculated.
     :return: The Burrows-Wheeler transform of the data.
     """
-    # Break the data into blocks:
-    blocks: list[BWTBlock] = [
-        BWTBlock(data[i:i + BWTBlock.MAX_BLOCK_SIZE], external_eof=METADATA_EOF) for i in
-        range(0, len(data), BWTBlock.MAX_BLOCK_SIZE)
-    ]
+    # If the data is empty return empty bytes:
+    if len(data) == 0:
+        return b''
 
-    # Result length calculation is as follows -
-    # Let N be the number of blocks, and D be a list where the ith element is the length of the ith block.
-    # Total length is the sum of:
-    # 1) EOFs for each block, each one byte long - N * 1 = N
-    # 2) One metadata EOF - 1
-    # 3) Every block's transformation, where each transformation's length is D[i] plus 1 internal EOF -
-    #    D[i] + 1 for i in [0, N) = sum(D) + N
-    #    Since the sum of every block's length is equal to the length of the total data, we can use that to save 
-    #    computation.
-    # In total: N + 1 + len(data) + N = 1 + 2N + len(data)
-    N = len(blocks)
-    total_D = len(data)
-    result: bytearray = bytearray(1 + (N << 1) + total_D)
+    # Compute suffix array of the data:
+    sorted_suffix_array = suffix_arr.build_sorted_suffix_array(data)
 
-    # Insert the blocks' EOFs to the start of the array, and their BWT to the data section of the array:
-    blocks_data_pointer = N + 1  # First non-metadata index
-    for block_idx, block in enumerate(blocks):
-        result[block_idx] = block.eof
+    # Compute the BWT of the data WITHOUT adding an EOF, however remember where an EOF should be:
+    eof_idx: int = None
+    bwt: bytearray = bytearray(len(data))
+    for byte_idx, sorted_suffix_offset in enumerate(sorted_suffix_array):
+        # If an EOF was found, we skipped an index since we didn't insert it:
+        if sorted_suffix_offset > 0:
+            bwt[byte_idx - int(eof_idx is not None)] = data[sorted_suffix_offset - 1]
+        else:
+            eof_idx = byte_idx
 
-        bwt: bytes = block.get_bwt()
-        transform_len = len(bwt)
-        result[blocks_data_pointer:blocks_data_pointer + transform_len] = bwt
-
-        blocks_data_pointer += transform_len
-
-    # Insert the metadata EOF:
-    result[N] = METADATA_EOF
-    return bytes(result)
+    # Add the BWT plus metadata:
+    return b''.join((get_bwt_metadata(eof_idx), bwt))
