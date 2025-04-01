bf243c127c44a5976c5d32a5e4f2934f88047b41
Yair5675 <yairziv5675@gmail.com>
2025-04-01 15:53:27 +0300

    Created a new file that will hold a testing script and will show us detailed data about the algorithms. For now only added todos

---
+++ tests/analysis.py
@@ -0,0 +1,9 @@
+# TODO:
+#  [ ] - Create some class that collects input size, compressed size, time it took to compress, memory usage data over
+#        time, compression ratio and space saving
+#  [ ] - Plot compression ratio relative to input size, color algorithms differently to make it more visually appealing
+#  [ ] - Plot memory usage of all algorithms in a single plot to compare them. since multiple files are compressed with
+#        the same algorithm, calculate the average memory usage graph for the same algorithm on multiple files and plot
+#        this
+#  [ ] - Rank algorithms for each corpora
+#  [ ] - Save results to a CSV
